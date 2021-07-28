9644598794b6cba194a36cbf38190324a6462c62
Joseph Toney <jdtoney@gmail.com>
2021-07-28 15:10:55 -0700

    Added tests for calculateStarRating and calculateCommentLength

--- interview-master/src/FeedbackApp.js
+++ interview-master/src/FeedbackApp.js
@@ -1,7 +1,62 @@
 const moment = require('moment')
+const MAX_LINE_LEN = 80;
+const SPACE_LEN = 1;
+const ELLIPSIS = '...';
+
 module.exports = class FeedbackApp {
     formatFeedback(feedback) {
+        /* Start here */
+        const { word, comment, date, rating } = feedback;
+        const formattedRating = this.formatRating(rating);
+        const formattedDate = this.formatDate(date);
 
+        /* construct formatted strings */
+        const formattedString = `${word}: ${comment}`;
+        const formattedStringWithRating = rating && `${formattedString} ${formattedRating}` || `${formattedString}`;
+        const formattedStringWithDate = `${formattedStringWithRating} (${formattedDate})`;
+
+        if (rating && formattedStringWithDate.length > 80 && formattedStringWithRating.length <= 80) {
+            /* Omit Date */
+            return formattedStringWithRating;
+        } else if (formattedStringWithRating.length > 80) {
+            /* truncate the comment */
+            const requiredCommentLen = this.calculateCommentLength(word.length, formattedRating.length);
+            return `${word}: ${comment.substring(0, requiredCommentLen)}${ELLIPSIS} ${formattedRating}`;
+        } else {
+            /* return the full formatted string */
+            return formattedStringWithDate;
+        }
     }
 
+    formatDate(date) {
+        return moment(date).format('l')
+    }
+
+    formatRating(rating) {
+        const star = '★';
+        const half = '½';
+        if (!rating) {
+            return '';
+        }
+        const starRating = this.calculateStarRating(rating);
+
+        if (Math.floor(starRating) !== starRating) {
+            return star.repeat(Math.floor(starRating)) + half;
+        } else {
+            return star.repeat(starRating);
+        }
+    }
+
+    calculateStarRating(rating) {
+        const starRating = (rating / 100) * 5;
+        if (starRating - parseInt(starRating) >= 0.5) {
+            return parseInt(starRating) + 0.5;
+        } else {
+            return parseInt(starRating);
+        }
+    }
+
+    calculateCommentLength(wordLength, ratingLength) {
+        return MAX_LINE_LEN - (ratingLength + (3 * SPACE_LEN) + wordLength + ELLIPSIS.length);
+    }
 };
--- interview-master/test/features/FeedbackApp.test.js
+++ interview-master/test/features/FeedbackApp.test.js
@@ -1,70 +1,129 @@
-let assert = require("assert");
-let FeedbackApp = require("../../src/FeedbackApp.js");
+const assert = require("assert");
+const FeedbackApp = require("../../src/FeedbackApp.js");
 
-describe("FeedbackApp", function() {
-  let app = new FeedbackApp();
+describe("FeedbackApp", () => {
+    const app = new FeedbackApp();
 
-  describe("#formatFeedback", function() {
-    it("should format the feedback to one line", function() {
-      assert.equal(
-        app.formatFeedback(swashbucklerFeedback),
-        "swashbuckler: Yar, a good word, matey! Shiver me timbers! ★★★★ (9/19/2019)"
-      );
+    describe("#formatFeedback", () => {
+        it("should format the feedback to one line", () => {
+            assert.equal(
+                app.formatFeedback(swashbucklerFeedback),
+                "swashbuckler: Yar, a good word, matey! Shiver me timbers! ★★★★ (9/19/2019)"
+            );
+        });
+
+        context("when formatted feedback would be over 80 characters", () => {
+            it("removes the date", () => {
+                assert.equal(
+                    app.formatFeedback(slubberFeedback),
+                    "slubber: Slubber sounds like slobber. So gross! More pretty words, please! ★½"
+                );
+            });
+        });
+
+        context("when date is removed and when formatted feedback is still over 80 characters", () => {
+            it("truncates the comment until the formatted feedback is 80 characters", () => {
+                assert.equal(
+                    app.formatFeedback(textlationshipFeedback),
+                    "textlationship: I'm in a textlationship with my friend! We text all the t... ★★★"
+                );
+            });
+        });
+
+        context("when the rating is omitted", () => {
+            it("omits the rating", () => {
+                assert.equal(
+                    app.formatFeedback(hyperbolicFeedback),
+                    "hyperbolic: How come teethpaste isn't in the dictionary? (8/22/2019)"
+                );
+            });
+        });
     });
 
-    describe("when formatted feedback would be over 80 characters", function() {
-      it("removes the date", function() {
-        assert.equal(
-          app.formatFeedback(slubberFeedback),
-          "slubber: Slubber sounds like slobber. So gross! More pretty words, please! ★½"
-        );
-      });
+    describe("#formatDate", () => {
+        it("should return a formatted date", () => {
+            assert.equal(
+                app.formatDate("22 Aug 2019 01:20:00 PST"),
+                "8/22/2019"
+            );
+        });
     });
 
-    describe("when date is removed and when formatted feedback is still over 80 characters", function() {
-      it("truncates the comment until the formatted feedback is 80 characters", function() {
-        assert.equal(
-          app.formatFeedback(textlationshipFeedback),
-          "textlationship: I'm in a textlationship with my friend! We text all the t... ★★★"
-        );
-      });
+    describe("#formatRating", () => {
+        context('when there is no rating', () => {
+            it('should return an empty string', () => {
+                assert.equal(
+                    app.formatRating(),
+                    ''
+                );
+            });
+        });
+
+        context('when there is a rating', () => {
+            it('should return the star rating with full stars correctly', () => {
+                assert.equal(
+                    app.formatRating(85),
+                    "★★★★"
+                );
+            });
+            it('should return the star rating with half stars correctly', () => {
+                assert.equal(
+                    app.formatRating(31),
+                    "★½"
+                );
+            });
+        });
     });
 
-    describe("when the rating is omitted", function() {
-      it("omits the rating", function() {
-        assert.equal(
-          app.formatFeedback(hyperbolicFeedback),
-          "hyperbolic: How come teethpaste isn't in the dictionary? (8/22/2019)"
-        );
-      });
+    describe("#calculateStarRating", () => {
+        it('should return the correct star rating with integers', () => {
+            assert.equal(
+                app.calculateStarRating(23),
+                1
+            );
+        });
+        it('should return the correct star rating with half values', () => {
+            assert.equal(
+                app.calculateStarRating(31),
+                1.5
+            );
+        });
+
     });
-  });
+
+    describe("#calculateCommentLength", () => {
+        it('should return the required comment length', () => {
+            assert.equal(
+                app.calculateCommentLength(7, 4),
+                63
+            );
+        })
+    });
 });
 
 let swashbucklerFeedback = {
-  word: "swashbuckler",
-  comment: "Yar, a good word, matey! Shiver me timbers!",
-  date: "19 Sep 2019 12:12:00 GMT",
-  rating: 85
+    word: "swashbuckler",
+    comment: "Yar, a good word, matey! Shiver me timbers!",
+    date: "19 Sep 2019 12:12:00 GMT",
+    rating: 85
 };
 
 let slubberFeedback = {
-  word: "slubber",
-  comment: "Slubber sounds like slobber. So gross! More pretty words, please!",
-  date: "10 Sep 2019 04:14:00 PST",
-  rating: 31
+    word: "slubber",
+    comment: "Slubber sounds like slobber. So gross! More pretty words, please!",
+    date: "10 Sep 2019 04:14:00 PST",
+    rating: 31
 };
 
 let textlationshipFeedback = {
-  word: "textlationship",
-  comment:
-    "I'm in a textlationship with my friend! We text all the time! I feel like it's every second! LMAO!",
-  date: "24 Aug 2019 20:54:00 GMT",
-  rating: 60
+    word: "textlationship",
+    comment: "I'm in a textlationship with my friend! We text all the time! I feel like it's every second! LMAO!",
+    date: "24 Aug 2019 20:54:00 GMT",
+    rating: 60
 };
 
 let hyperbolicFeedback = {
-  word: "hyperbolic",
-  comment: "How come teethpaste isn't in the dictionary?",
-  date: "22 Aug 2019 01:20:00 PST"
+    word: "hyperbolic",
+    comment: "How come teethpaste isn't in the dictionary?",
+    date: "22 Aug 2019 01:20:00 PST"
 };
